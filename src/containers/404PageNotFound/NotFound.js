9fb02e3108b27d76956f60728344d0d8c14b1868
volkswg <volkswg@hotmail.com>
2021-05-26 19:25:17 +0700

    update not found page

---
+++ src/containers/404PageNotFound/NotFound.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "reactstrap";
+import { Link } from "react-router-dom";
+import classes from "./NotFound.module.css";
+
+const NotFound = () => {
+  return (
+    <Container style={{ paddingTop: "70px", textAlign: "center" }}>
+      <div style={{ fontSize: "10rem", fontWeight: "bold", color: "#3a3a3a" }}>404</div>
+      <div style={{ fontSize: "2rem" }}>Page Not Found</div>
+      <Link class={classes.BackLink} to={"/"}>
+        &lt; Back to Home Page
+      </Link>
+    </Container>
+  );
+};
+
+export default NotFound;
