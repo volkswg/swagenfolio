66e47bdd14ef54d092b8079c600bcfdc40a3d1f3
volkswg <volkswg@hotmail.com>
2022-02-19 18:15:43 +0700

    optimize image using in contact section

--- src/components/BasicInfo/ContactList/ContactList.js
+++ src/components/BasicInfo/ContactList/ContactList.js
@@ -3,39 +3,31 @@ import CardElem from "./CardElem/CardElem";
 
 const ContactList = () => {
   return (
-    <div style={{ display: "flex" }}>
-      <div>
-        <CardElem
-          url={"https://www.facebook.com/Saranpat.Funkaew"}
-          logo_src={require("../../../assets/image/contact/fb-logo.png")}
-          contract_type="Facebook"
-          contract_info="Saranpat.Funkaew"
-        />
-      </div>
-      <div>
-        <CardElem
-          url={"https://github.com/volkswg"}
-          logo_src={require("../../../assets/image/contact/gh-logo.png")}
-          contract_type="GitHub"
-          contract_info="volkswg"
-        />
-      </div>
-      <div>
-        <CardElem
-          url={"https://www.linkedin.com/in/saranpat-funkaew-64a119154/"}
-          logo_src={require("../../../assets/image/contact/linkedin-logo.png")}
-          contract_type="Linkedin"
-          contract_info="Saranpat Funkaew"
-        />
-      </div>
-      <div>
-        <CardElem
-          url={null}
-          logo_src={require("../../../assets/image/contact/telC-logo.png")}
-          contract_type="Tel."
-          contract_info="081-6668881"
-        />
-      </div>
+    <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+      <CardElem
+        url={"https://www.facebook.com/Saranpat.Funkaew"}
+        logo_src="image/contact/fb-logo.png"
+        contract_type="Facebook"
+        contract_info="Saranpat.Funkaew"
+      />
+      <CardElem
+        url={"https://github.com/volkswg"}
+        logo_src="image/contact/gh-logo.png"
+        contract_type="GitHub"
+        contract_info="volkswg"
+      />
+      <CardElem
+        url={"https://www.linkedin.com/in/saranpat-funkaew-64a119154/"}
+        logo_src="image/contact/linkedin-logo.png"
+        contract_type="Linkedin"
+        contract_info="Saranpat Funkaew"
+      />
+      <CardElem
+        url={null}
+        logo_src="image/contact/telC-logo.png"
+        contract_type="Tel."
+        contract_info="081-6668881"
+      />
     </div>
   );
 };
