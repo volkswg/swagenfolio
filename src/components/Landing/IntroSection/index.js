04f6e3139705afae5aabd3b5aa075a266f600b88
Saranpat <saranpat.fu@easysunday.com>
2024-11-26 17:07:24 +0700

    update intro section

--- src/components/Landing/IntroSection/index.js
+++ src/components/Landing/IntroSection/index.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import DownloadIcon from '../../svg-icon/download-icon';
-import style from './IntroSection.module.css'
+import React from "react";
+import DownloadIcon from "../../svg-icon/download-icon";
+import style from "./IntroSection.module.css";
 
 const IntroSection = () => {
-  return (  <div className={style.ContentContainer}>
-    <span className={style.FadedText}>Full Stack Developer</span>
-    <h1 className={style.Title}>SARANPAT FUNKAEW</h1>
-    <span className={style.FadedText}>
-      I graduated from King Mongkut's University of Technology Thonburi
-      (KMUTT). Computer Engineering.
-    </span>
-    <div className={style.DownloadCVContainer}>
-      <a href="/resume/SaranpatFunkaewCV.pdf" target={"_blank"}>
-        <DownloadIcon className={style.DownloadIcon} />
-        <div className={style.CVText}>Resume</div>
-      </a>
+  return (
+    <div className={style.ContentContainer}>
+      <span className={style.FadedText}>Full Stack Developer</span>
+      <h1 className={style.Title}>SARANPAT FUNKAEW</h1>
+      <span className={style.FadedText}>
+        I specialize in leveraging JavaScript frameworks such as React.js,
+        Next.js, Vue.js, and Node.js to develop modern applications. My
+        expertise includes database design, SQL query optimization, webpage
+        performance optimization, and migrating legacy systems to modern
+        solutions.
+      </span>
+      <div className={style.DownloadCVContainer}>
+        <a href="/resume/SaranpatFunkaew2024.pdf" target={"_blank"}>
+          <DownloadIcon className={style.DownloadIcon} />
+          <div className={style.CVText}>Resume</div>
+        </a>
+      </div>
     </div>
-  </div>);
-}
- 
+  );
+};
+
 export default IntroSection;
